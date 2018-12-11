b0c93283cd9995bd6b74efc75a1756b6a00642d8
hodayamar <hodayamar2000@gmail.com>
2018-12-11 15:43:26 +0200

    small changes

--- fix_csv.py
+++ fix_csv.py
@@ -7,16 +7,21 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 fixed = open('data/fixed.csv', 'a')
-broken_lines = []
+broken_lines = 0
 
 with open('data/oddetect.csv') as f:
+
     for index, line in enumerate(f):
+
         line_B = line.split(',')
-
         if len(line_B) == 14:
-            fixed.write(line)
-        else:
-            logger.info(f"Broken line #{index}")
-            broken_lines.append(index)
 
-    logger.info(f"Broken lines #{len(broken_lines)}, Fixed lines {index - len(broken_lines)}")
+            fixed.write(line)
+
+        else:
+
+            logger.info(f"Broken line #{index}")
+
+            broken_lines += 1
+
+    logger.info(f"Broken lines #{broken_lines}, Fixed lines {index - broken_lines}")
