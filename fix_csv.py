f948939af0ac8c3184476bf9f6565d7d5e6ec8a7
hodayamar <hodayamar2000@gmail.com>
2018-12-11 15:20:00 +0200

    part 0 done. 2997978 valid lines 1228 invalid lines.

---
+++ fix_csv.py
@@ -0,0 +1,24 @@
+import logging
+import csv
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+fixed = open('data/fixed.csv', 'a')
+broken_lines = []
+
+with open('data/oddetect.csv') as f:
+    fixed.write(f.readline())
+    for index, line in enumerate(f):
+        line_B = line.split(',')
+        if len(line_B) == 14:
+            fixed.write(line)
+        else:
+            # print(line)
+            logger.info(f"Broken line #{index}")
+            broken_lines.append(index)
+
+    logger.info(f"Broken lines #{len(broken_lines)}, Fixed lines {index - len(broken_lines)}")
